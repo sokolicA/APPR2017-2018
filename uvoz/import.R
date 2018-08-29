13de1b08dc6537463a1928992614012707901c2a
sokolicA <andrejsokolicc@gmail.com>
2018-08-29 18:08:40 +0200

    ko

--- uvoz/import.R
+++ uvoz/import.R
@@ -17,20 +17,26 @@ BTC <- do.call(cbind, lapply(uvoz, function(x) read.csv(file = x,
 BTC <- BTC[!duplicated(as.list(BTC))]
 
 # imenujemo stolpce
-colnames(BTC) <- c("Datum", "Cena($)", "Tržna kapitalizacija", "Število transakcij", "Skupno število transakcij", "Dnevni promet", "Skupno število BTC")
+colnames(BTC) <- c("Datum", "Cena", "Trzna_kap", "St_transakcij", "Skupno_st_transakcij", "Promet", "BTC_v_obtoku")
 
 BTC[,2:7] <- lapply(BTC[,2:7], function(x) round(x, digits =  2))
 
 
 #odstranimo uro
 
-BTC[,1] <- gsub(pattern = "00:00:00", replacement = "", BTC[,1])
-        
-# po valutah vir: https://data.bitcoinity.org/markets/volume/30d?c=e&t=b
+BTC[,1] <- gsub(pattern = " 00:00:00", replacement = "", BTC[,1])
+
+#spremenimo v datume
+
+BTC[,1] <- as.Date(BTC[,1], format = "%Y-%m-%d")
+
+# trgovanje po valutah vir: https://data.bitcoinity.org/markets/volume/30d?c=e&t=b
 
 val <- read.csv("../podatki/bitcoinity_data.csv")
-val[,1] <- gsub(pattern = "00:00:00 UTC", replacement = "", x = val[,1])
+val[,1] <- gsub(pattern = " 00:00:00 UTC", replacement = "", x = val[,1])
 val[,2:11] <- lapply(val[,2:11], function(x) round(x, digits =  2))
+
+val[,1] <- as.Date(val[,1], format="%Y-%m-%d")
 
 
 # bitcoin bankomati po svetu
@@ -48,20 +54,22 @@ atm[,2] <- gsub(" .*", "", atm[,2])
 atm[,2] <- as.integer(as.character(atm[,2]))
 atm[,1] <- as.character(as.factor(atm[,1]))
 atm[1,1] <- "USA"
+atm[grep("United Kingdom", atm[,1]),1] <- "UK"
 
 
-# GRAFI BANKOMATOV NISO VSA IMENA ISTA
-# library(ggplot2)
-# library(dplyr)
-# 
-# data <- map_data('world') %>% group_by(region) %>% summarise() %>% print(n = Inf)
-# data <- map_data("world")
-# hf <- left_join(data, atm, by = c('region' = 'Država'))
-# 
-# ggplot(data = hf, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = `Število lokacij`) )+
-#         scale_colour_brewer()+ 
-#         theme_bw()
-# 
+#GRAFI BANKOMATOV NISO VSA IMENA ISTA
+library(ggplot2)
+library(dplyr)
+library(maps)
+
+data <- map_data('world') %>% group_by(region) %>% summarise() %>% print(n = Inf)
+data <- map_data("world")
+hf <- left_join(data, atm, by = c('region' = 'Država'))
+
+ggplot(data = hf, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = `Število lokacij`) )+
+        scale_colour_brewer()+
+        theme_bw()
+
 
 
 
---
+++ vizualizacija.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+
+source(file = "../uvoz/import.R")
+
+
+
+ggplot(data = BTC, aes(datum, cena))
